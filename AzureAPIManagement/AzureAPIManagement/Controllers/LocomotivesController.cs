19c97f3ab1e786eae267a37fb13f900e5a7f8699
ITS-GROUP\Alpers <stefan.alpers@its-service.de>
2019-03-24 22:21:17 +0100

    code

---
+++ AzureAPIManagement/AzureAPIManagement/Controllers/LocomotivesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureAPIManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAPIManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocomotivesController : ControllerBase
+    {
+        #region Fields
+
+        private static List<Locomotive> _Locomotives = new List<Locomotive>()
+        {
+            new Locomotive
+            {
+                ModelRange = "103",
+                Type = "E",
+                Length = 19500,
+                ServiceWeight = 114,
+                Vmax = 200
+            },
+            new Locomotive
+            {
+                ModelRange = "01",
+                Type = "Dampf",
+                Length = 23940,
+                ServiceWeight = 109,
+                Vmax = 120
+            },
+            new Locomotive
+            {
+                ModelRange = "V200",
+                Type = "Diesel",
+                Length = 18470,
+                ServiceWeight = 80,
+                Vmax = 140
+            }
+        };
+
+        #endregion
+
+        // GET: api/Locomotives
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Locomotive>), 200)]
+        public async Task<ActionResult<IEnumerable<Locomotive>>> Get()
+        {
+            return Ok(new ActionResult<IEnumerable<Locomotive>>(_Locomotives));
+        }
+
+        // GET: api/Locomotives/V200
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Locomotive), 200)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult<Locomotive>> Get(string id)
+        {
+            Locomotive locomotive = null;
+
+            try
+            {
+                locomotive = _Locomotives.First(l => l.ModelRange == id);
+            }
+            catch (ArgumentNullException)
+            {
+                return NotFound();
+            }
+
+            return Ok(locomotive);
+        }
+
+        // POST: api/Locomotives
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Locomotives/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/Locomotives/V200
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public async Task<ActionResult> Delete(string id)
+        {
+            try
+            {
+                var itemToDelete = _Locomotives.First(l => l.ModelRange == id);
+                _Locomotives.Remove(itemToDelete);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ AzureAPIManagement/AzureAPIManagement/Models/Locomotive.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace AzureAPIManagement.Models
+{
+    public class Locomotive
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string ModelRange { get; set; }
+
+        public string Type { get; set; }
+
+        public int Length { get; set; }
+
+        public int ServiceWeight { get; set; }
+
+        public int Vmax { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
